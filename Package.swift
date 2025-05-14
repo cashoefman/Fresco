849b958c113fe19ffbd48cc74a915503242b3e41
Cas Hoefman <cashoefman@gmail.com>
2025-05-14 18:27:52 -0400

    CI Workflow Updated for lower SwiftTools Version 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-//  swift-tools-version: 6.1
+//  swift-tools-version: 6.0
 //  The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Fresco",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v18),
         .macOS(.v10_15)
     ],
     products: [
